7d8050647376850a36687ab96a353d68836536b1
cwadeking <charles.w.king.cwk@gmail.com>
2019-08-21 15:19:45 -0500

    first attempt at creating a function to calculate the dice selections from the userimput

--- dicegame.js
+++ dicegame.js
@@ -1,6 +1,10 @@
 // alert("Welcome to Dice Bowling!")
 // console.log(prompt("What is your name?"))
-// document.write("Welcome to Dice Bowling.  Dice Bowling is a lot like regular bowling, except ZERO physical skill is required.  Just enter the 'roll' command and let us do the work for you.  After rolling 6 different dice (4, 6, 8, 10, 12, and 20-sided), on your first roll, you will need to select 3 (or more) dice that are equal to/divisible by 10.  If you can do that, you've just rolled a strike.  If you don't have 3 (or more) dice that are equal to/divisible by 10, then select as many dice as you want that are less than 10 for your initial roll.  The sum of those dice will be entered and you'll be ready for your second roll")
+// document.write("Welcome to Dice Bowling.  Dice Bowling is a lot like regular bowling, except ZERO physical skill is required.  
+//Just click a few things and let us do the work for you.  After rolling 6 different dice (4, 6, 8, 10, 12, and 20-sided), 
+//on your first roll, you will need to select 2 dice that are equal to by 10.  If you can do that, you've just 
+//rolled a strike.  If you don't have 2 dice that are equal to by 10, then select two dice that you want that
+// are less than 10 for your initial roll.  The sum of those dice will be entered and you'll be ready for your second roll")
 
 function rollDice(min,max){
 	return (Math.floor(Math.random(min) * max) + 1);
@@ -41,7 +45,6 @@ function defaultScore(){
 	else{
 		knockedPins = diceSelect() + diceSelect()
 	}
-	// while(diceSelect1() + diceSelect2() + diceSelect3() + diceSelect4() + diceSelect5() + diceSelect6() <= 10);
 	return knockedPins;
 }
 
@@ -72,144 +75,19 @@ function diceSelect(){
 	}
 }
 
-// function diceSelect2(){
-// 	let selectedRolls = [dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix];
-// 	let userInput = prompt("Which dice do you choose? (one at a time)");
-// 	if(userInput == 1){
-// 	console.log(selectedRolls[0]);
-// 	}
-// 	else if(userInput == 2){
-// 	console.log(selectedRolls[1]);
-// 	}
-// 	else if(userInput == 3){
-// 	console.log(selectedRolls[2]);
-// 	}
-// 	else if(userInput == 4){
-// 	console.log(selectedRolls[3]);
-// 	}
-// 	else if(userInput == 5){
-// 	console.log(selectedRolls[4]);
-// 	}
-// 	else if(userInput == 6){
-// 	console.log(selectedRolls[5]);
-// 	}
-// 	else{
-// 	console.log(prompt("That is not an accurate selection."))
-// 	}
-// }
-
-// function diceSelect3(){
-// 	let selectedRolls = [dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix];
-// 	let userInput = prompt("Which dice do you choose? (one at a time)");
-// 	if(userInput == 0){
-// 		return;
-// 	}
-
-
-// 	if(userInput == 1){
-// 	console.log(selectedRolls[0]);
-// 	}
-// 	else if(userInput == 2){
-// 	console.log(selectedRolls[1]);
-// 	}
-// 	else if(userInput == 3){
-// 	console.log(selectedRolls[2]);
-// 	}
-// 	else if(userInput == 4){
-// 	console.log(selectedRolls[3]);
-// 	}
-// 	else if(userInput == 5){
-// 	console.log(selectedRolls[4]);
-// 	}
-// 	else if(userInput == 6){
-// 	console.log(selectedRolls[5]);
-// 	}
-// 	else{
-// 	console.log(prompt("That is not an accurate selection."))
-// 	}
-// }
-
-// function diceSelect4(){
-// 	let selectedRolls = [dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix];
-// 	let userInput = prompt("Which dice do you choose? (one at a time)");
-// 	if(userInput == 1){
-// 	console.log(selectedRolls[0]);
-// 	}
-// 	else if(userInput == 2){
-// 	console.log(selectedRolls[1]);
-// 	}
-// 	else if(userInput == 3){
-// 	console.log(selectedRolls[2]);
-// 	}
-// 	else if(userInput == 4){
-// 	console.log(selectedRolls[3]);
-// 	}
-// 	else if(userInput == 5){
-// 	console.log(selectedRolls[4]);
-// 	}
-// 	else if(userInput == 6){
-// 	console.log(selectedRolls[5]);
-// 	}
-// 	else{
-// 	console.log(prompt("That is not an accurate selection."))
-// 	}
-// }
-
-// function diceSelect5(){
-// 	let selectedRolls = [dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix];
-// 	let userInput = prompt("Which dice do you choose? (one at a time)");
-// 	if(userInput == 1){
-// 	console.log(selectedRolls[0]);
-// 	}
-// 	else if(userInput == 2){
-// 	console.log(selectedRolls[1]);
-// 	}
-// 	else if(userInput == 3){
-// 	console.log(selectedRolls[2]);
-// 	}
-// 	else if(userInput == 4){
-// 	console.log(selectedRolls[3]);
-// 	}
-// 	else if(userInput == 5){
-// 	console.log(selectedRolls[4]);
-// 	}
-// 	else if(userInput == 6){
-// 	console.log(selectedRolls[5]);
-// 	}
-// 	else{
-// 	console.log(prompt("That is not an accurate selection."))
-// 	}
-// }
-// function diceSelect6(){
-// 	let selectedRolls = [dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix];
-// 	let userInput = prompt("Which dice do you choose? (one at a time)");
-// 	if(userInput == 1){
-// 	console.log(selectedRolls[0]);
-// 	}
-// 	else if(userInput == 2){
-// 	console.log(selectedRolls[1]);
-// 	}
-// 	else if(userInput == 3){
-// 	console.log(selectedRolls[2]);
-// 	}
-// 	else if(userInput == 4){
-// 	console.log(selectedRolls[3]);
-// 	}
-// 	else if(userInput == 5){
-// 	console.log(selectedRolls[4]);
-// 	}
-// 	else if(userInput == 6){
-// 	console.log(selectedRolls[5]);
-// 	}
-// 	else{
-// 	console.log(prompt("That is not an accurate selection."))
-// 	}
-// }
-///create a function that allows player to select which dice to keep for calc.  Select >= 2 dice.///
-
-// function calcRoll(selectedRolls){
-// //create a function to calculate input (dice selection) and return sums to the scoreboard//
-// }
+function calcRoll1(){
+	if (diceSelect() + diceSelect() < 10){
+		return = rollOneScore;
+		console.log()
+	}
+	else if (diceSelect() + diceSelect() =10){
+		return = strikeFrame;
+		console.log()
+	}
+	else {
+		prompt("You're trying to cheat!")
+	}
+}
 
 // function scoreRolls(){
 // //create function to take in calcRoll inputs into two rolls per frame (unless strike).  Strike automatically moves frame over for next roll//
