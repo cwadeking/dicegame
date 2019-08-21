3c7a3102bf917b21bbcc1a23fd89c10f7065d9ca
cwadeking <charles.w.king.cwk@gmail.com>
2019-08-21 15:45:53 -0500

    deleted a lot of excess comments and disabled initial prompts.

--- dicegame.js
+++ dicegame.js
@@ -27,66 +27,66 @@ console.log("Die 6 is=   " + dieSix)
 
 function defaultScore(){
 	let knockedPins = 0;
-	if(dieOne + dieTwo + dieThree + dieFour + dieFive + dieSix >= 50){
-		console.log("Gutterball")
-		knockedPins = 0;
-		rollDice()
-		console.log()
-	//figure out how to enter in a zero for first roll in that frame//
-	}
-	else if(dieOne + dieTwo == 2 || dieOne + dieThree == 2 || dieOne + dieFour == 2 || dieOne + dieFive == 2 || dieOne + dieSix == 2 || dieTwo + dieThree == 2 || dieTwo + dieFour == 2 || dieTwo + dieFive == 2 || dieTwo + dieSix == 2 || dieThree + dieFour == 2 || dieThree + dieFive == 2 || dieThree + dieSix == 2 || dieFour + dieFive == 2 || dieFour + dieSix == 2 || dieFive + dieSix == 2){
-		console.log("Split")
-		knockedPins = 8;
-		rollDice()
-		console.log()
-	//figure out how to put an 8 down for that frame//
-		//When setting up the second roll, should be able to cut and paste this//
-	}
-	else{
-		knockedPins = diceSelect() + diceSelect()
-	}
-	return knockedPins;
+		if(dieOne + dieTwo + dieThree + dieFour + dieFive + dieSix >= 50){
+			console.log("Gutterball")
+			knockedPins = 0;
+			rollDice()
+			console.log()
+		//figure out how to enter in a zero for first roll in that frame//
+		}
+		else if(dieOne + dieTwo == 2 || dieOne + dieThree == 2 || dieOne + dieFour == 2 || dieOne + dieFive == 2 || dieOne + dieSix == 2 || dieTwo + dieThree == 2 || dieTwo + dieFour == 2 || dieTwo + dieFive == 2 || dieTwo + dieSix == 2 || dieThree + dieFour == 2 || dieThree + dieFive == 2 || dieThree + dieSix == 2 || dieFour + dieFive == 2 || dieFour + dieSix == 2 || dieFive + dieSix == 2){
+			console.log("Split")
+			knockedPins = 8;
+			rollDice()
+			console.log()
+		}
+		else{
+			knockedPins = diceSelect() + diceSelect()
+		}
+		return knockedPins;
 }
 
 
 function diceSelect(){
 	let selectedRolls = [dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix];
 	let userInput = prompt("Which dice do you choose? (one at a time)");
-	if(userInput == 1){
-	console.log(selectedRolls[0]);
-	}
-	else if(userInput == 2){
-	console.log(selectedRolls[1]);
-	}
-	else if(userInput == 3){
-	console.log(selectedRolls[2]);
-	}
-	else if(userInput == 4){
-	console.log(selectedRolls[3]);
-	}
-	else if(userInput == 5){
-	console.log(selectedRolls[4]);
-	}
-	else if(userInput == 6){
-	console.log(selectedRolls[5]);
-	}
-	else{
-	console.log(prompt("That is not an accurate selection."))
-	}
+		if(userInput == 1){
+		console.log(selectedRolls[0]);
+		}
+		else if(userInput == 2){
+		console.log(selectedRolls[1]);
+		}
+		else if(userInput == 3){
+		console.log(selectedRolls[2]);
+		}
+		else if(userInput == 4){
+		console.log(selectedRolls[3]);
+		}
+		else if(userInput == 5){
+		console.log(selectedRolls[4]);
+		}
+		else if(userInput == 6){
+		console.log(selectedRolls[5]);
+		}
+		else{
+		console.log(prompt("That is not an accurate selection."))
+		}
 }
 
-function calcRoll1(){
-	if (diceSelect() + diceSelect() < 10){
-		return = rollOneScore;
-		console.log()
-	}
-	else if (diceSelect() + diceSelect() =10){
-		return = strikeFrame;
-		console.log()
-	}
-	else {
-		prompt("You're trying to cheat!")
-	}
+function calcRoll(){
+	let rollOneScore = 0;
+	let strikeFrame = 0;
+		if (diceSelect() + diceSelect() <= 9){
+		return rollOneScore;
+		console.log();
+		}
+		else if (diceSelect() + diceSelect() = 10){
+		return strikeFrame;
+		console.log();
+		}
+		else {
+		prompt("You're trying to cheat!");
+		}
 }
 
 // function scoreRolls(){
