90e02d21f6dbe9357fb6146de6e4bd25237fa204
cwadeking <charles.w.king.cwk@gmail.com>
2019-09-22 20:05:08 -0500

    working on updating functions with better knowledge and functionality.  changed around the order for better flow.  starting to test

--- dicegame.js
+++ dicegame.js
@@ -1,10 +1,12 @@
-// alert("Welcome to Dice Bowling!")
-// console.log(prompt("What is your name?"))
-// document.write("Welcome to Dice Bowling.  Dice Bowling is a lot like regular bowling, except ZERO physical skill is required.  
-//Just click a few things and let us do the work for you.  After rolling 6 different dice (4, 6, 8, 10, 12, and 20-sided), 
-//on your first roll, you will need to select 2 dice that are equal to by 10.  If you can do that, you've just 
-//rolled a strike.  If you don't have 2 dice that are equal to by 10, then select two dice that you want that
-// are less than 10 for your initial roll.  The sum of those dice will be entered and you'll be ready for your second roll")
+alert("Welcome to Dice Bowling!")
+console.log(prompt("What is your name?"))
+document.write("Welcome to Dice Bowling.  Dice Bowling is a lot like regular bowling, except ZERO physical skill is required. + \n Just click a few things and let us do the work for you.  After rolling 6 different dice (4, 6, 8, 10, 12, and 20-sided), \n on your first roll, you will need to select 2 dice that are equal to 10.  If you can do that, you've just \n rolled a strike.  If you don't have 2 dice that are equal to by 10, then select two dice that you want that\n are less than 10 for your initial roll.  The sum of those dice will be entered and you'll be ready for your second roll")
+
+
+
+
+
+
 
 function rollDice(min,max){
 	return (Math.floor(Math.random(min) * max) + 1);
