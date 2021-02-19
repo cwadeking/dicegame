3ba3e9a2bc09c9ec7fddb7fd8be6db3ea5b68ea7
cwadeking <charles.w.king.cwk@gmail.com>
2021-02-18 20:35:51 -0600

    worked out some bugs that were creating infinite loops.  created a checkIfSpareIsPossible method.  The second dice roll turned out to be too much compared to the first.  Made the frame value too much.  Working on more single responsibility methods now.

--- dicegame.js
+++ dicegame.js
@@ -1,18 +1,21 @@
-alert("Welcome to Dice Bowling!");
-console.log(prompt("What is your name?"));
-document.write(`Welcome to Dice Bowling.  Dice Bowling is a lot like regular bowling, except ZERO physical skill is required.
-                 \n Just click a few things and let us do the work for you.  After rolling 6 different dice (4, 6, 8, 10, 12, and 20-sided),
-                  \n on your first roll, we'll check if there are two dice that equal 10.  If so, you've just 
-                  \n rolled a strike.  If you don't have 2 dice that equal 10, then select two dice that you want that
-                  \n are less than 10 for your initial roll.  The sum of those dice will be entered and you'll be ready for your 
-                  \n second roll. On your second roll, you will have to choose one more die to complete the frame for a spare, or 
-                  \n leave the frame open and your score entered!`);
+function displayGameRules(){
+    document.write(`Welcome to Dice Bowling.  Dice Bowling is a lot like regular bowling, except ZERO physical skill is required.
+    \n Just click a few things and let us do the work for you.  After rolling 6 different dice (4, 6, 8, 10, 12, and 20-sided),
+    \n on your first roll, we'll check if there are two dice that equal 10.  If so, you've just 
+    \n rolled a strike.  If you don't have 2 dice that equal 10, then select two dice that you want that
+    \n are less than 10 for your initial roll.  The sum of those dice will be entered and you'll be ready for your 
+    \n second roll. On your second roll, you will have to choose one more die to complete the frame for a spare, or 
+    \n leave the frame open and your score entered!`);
+}
 
 
 
-runGame();
+//runGame();
 
-    function runGame(){
+function runGame(){
+   // alert("Welcome to Dice Bowling!");
+    //console.log(prompt("What is your name?"));
+    //displayGameRules();
         let gameArray = [
             createStandardFrameObject(rollTwo(rollOne())),
             createStandardFrameObject(rollTwo(rollOne())),
@@ -71,6 +74,19 @@ runGame();
 
     
 
+    function checkValuesRollOne(diceArray){
+        if(splitFrameOrPickupYesOrNo(diceArray)){
+            return createShotObject("split", 8);
+        }else if(gutterBallCheck(diceArray)){
+            return createShotObject("gutter ball", 0);
+        }else if(strikeCheck(diceArray)){
+            return createShotObject("strike", 10);
+        }else{
+            return diceSelect(diceArray);
+        }
+
+    }
+
     function checkValuesRollTwo(diceArray,firstShot){
         if(firstShot.ShotType === "split"){
             if(splitFrameOrPickupYesOrNo(diceArray)){
@@ -83,19 +99,6 @@ runGame();
         }else{
             return diceSelect(diceArray, firstShot.ShotValue);
         }
-    }
-
-    function checkValuesRollOne(diceArray){
-        if(splitFrameOrPickupYesOrNo(diceArray)){
-            return createShotObject("split", 8);
-        }else if(gutterBallCheck(diceArray)){
-            return createShotObject("gutter ball", 0);
-        }else if(strikeCheck(diceArray)){
-            return createShotObject("strike", 10);
-        }else{
-            return diceSelect(diceArray);
-        }
-
     }
 
     function rollSingleDie(sides){
@@ -111,6 +114,8 @@ runGame();
         let dieFour = rollSingleDie(10);
         let dieFive = rollSingleDie(12);
         let dieSix = rollSingleDie(20);
+
+        //need a function to create dice collection objects?
         let diceCollection = [
             {
                 diceNumber: "1",
@@ -202,24 +207,93 @@ runGame();
         return checkArrayCombosForATenValue(arrayCombos);
     }
 
-    function diceSelect(rolledDice, valueOfFirstRoll){
-        let tempPinValue = 0;
-        if(valueOfFirstRoll){
-            tempPinValue = valueOfFirstRoll;
+
+    function canASpareBeMade(valueOfFirstRoll, allDiceValues){
+        for(let i = 0; i < allDiceValues.length; i++){
+            if(valueOfFirstRoll + allDiceValues[i].value <= 10){
+                return true;
+            }
         }
+        return false;
+    }
+
+    function chooseTwoDistinctDice(){
+        let tempPinValue = 0;
+        let counter = 0;
+        let firstChoice;
+       
+        while(tempPinValue > 10 || counter < 2){
+            if(tempPinValue > 10){
+                tempPinValue = 0;
+            }
+            //need to create validation for different types of prompts.
+            let userInput = prompt("Which dice do you choose? (one at a time)");
+            if(userInput === firstChoice){
+                continue;
+            }
+            //this switch case needs to be another function
+            switch(userInput){
+                case "1":
+                        tempPinValue += rolledDice[0].value;
+                        counter++;
+                        firstChoice = "1";
+                        break;
+                case "2":
+                        tempPinValue += rolledDice[1].value;
+                        counter++;
+                        firstChoice = "2";
+                        break;
+                case "3":
+                        tempPinValue += rolledDice[2].value;
+                        counter++;
+                        firstChoice += "3";
+                        break;
+                case "4":
+                        tempPinValue += rolledDice[3].value;
+                        counter++;
+                        firstChoice = "4";
+                        break;
+                case "5":
+                        tempPinValue += rolledDice[4].value;
+                        counter++;
+                        firstChoice = "5";
+                        break;
+                case "6":
+                        tempPinValue += rolledDice[5].value;
+                        counter++;
+                        firstChoice = "6";
+                        break;
+                default:
+                        console.log(prompt("That is not an accurate selection."))
+            }
+
+    }
+
+
+
+    function diceSelectFirstRoll(rolledDice){
+        //need to create separate functions for better flow
+        //taking in the value of the first roll, I need to see if any of the values of the dice are less than what is allowed to make a spare
+        //if not, I need to create an empty shot value
+        //
+        let tempPinValue = 0;
+        
         let counter = 0;
         if(tempPinValue){
             counter = 1;
         }
         let firstChoice;
+        //need to create constraint values for these magic numbers
         while(tempPinValue > 10 || counter < 2){
             if(tempPinValue > 10){
                 tempPinValue = 0;
             }
+            //need to create validation for different types of prompts.
             let userInput = prompt("Which dice do you choose? (one at a time)");
             if(userInput === firstChoice){
                 continue;
             }
+            //this switch case needs to be another function
             switch(userInput){
                 case "1":
                         tempPinValue += rolledDice[0].value;
@@ -258,3 +332,12 @@ runGame();
         return createShotObject("basic", tempPinValue);
     }    
     
+
+    function diceSelectSecondRoll(){
+        if(valueOfFirstRoll){
+            tempPinValue = valueOfFirstRoll;
+            if(!canASpareBeMade(tempPinValue, rolledDice)){
+                return createShotObject("open", 0);
+            }
+        }
+    }
